d90a255ba68ef3e9227d392db8a85d2011d92a51
Isaiah <greatpup1940@gmail.com>
2020-09-26 21:39:36 -0400

    Added cors

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const sendEmail = require('./email')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
+
 
 app.use(express.json())
+
+
+
 
 app.get('/', (req, res, next) => {
     res.status(200).json({
